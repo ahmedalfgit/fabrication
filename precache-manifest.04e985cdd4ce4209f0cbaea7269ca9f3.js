c6cfa1baf38088e4d41866873fe30e6cf3275625
ahmed <ahmed@serpentix.com>
2021-03-10 09:49:33 -0700

    Updates

---
+++ precache-manifest.04e985cdd4ce4209f0cbaea7269ca9f3.js
@@ -0,0 +1,210 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebb48c4523422ad64b3baffcf05e4106",
+    "url": "/index.html"
+  },
+  {
+    "revision": "363789e4a1c90dad5a64",
+    "url": "/static/css/2.ad676774.chunk.css"
+  },
+  {
+    "revision": "a89435eeccd2164191a4",
+    "url": "/static/css/main.1d3088ce.chunk.css"
+  },
+  {
+    "revision": "363789e4a1c90dad5a64",
+    "url": "/static/js/2.3f585720.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.3f585720.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a89435eeccd2164191a4",
+    "url": "/static/js/main.2898826f.chunk.js"
+  },
+  {
+    "revision": "64cf2905138960a8b2d9",
+    "url": "/static/js/runtime-main.298f7efd.js"
+  },
+  {
+    "revision": "a0268ba1cb292ca7889b4f6c61c980a8",
+    "url": "/static/media/Goform.a0268ba1.png"
+  },
+  {
+    "revision": "f3d7b462b1b57964cc313dbcb33b9122",
+    "url": "/static/media/MIG1.f3d7b462.jpg"
+  },
+  {
+    "revision": "15f81cf2caddd196f7384e68854a8e27",
+    "url": "/static/media/MIG2.15f81cf2.jpg"
+  },
+  {
+    "revision": "e3f64583e24dd04ef06e0c2c398bd24c",
+    "url": "/static/media/MIG3.e3f64583.jpg"
+  },
+  {
+    "revision": "dc0df5d9657342264777d33bc65cecea",
+    "url": "/static/media/MIG4.dc0df5d9.jpg"
+  },
+  {
+    "revision": "b5051398fcae1f30a61b6efe0c5de043",
+    "url": "/static/media/Robotic1.b5051398.jpg"
+  },
+  {
+    "revision": "b7e7869ae0d2feb9c73bcb704abcd842",
+    "url": "/static/media/Robotic2.b7e7869a.jpg"
+  },
+  {
+    "revision": "69294e1c9c898634e23aa808137bca17",
+    "url": "/static/media/Robotic3.69294e1c.jpg"
+  },
+  {
+    "revision": "e384794db2457fa87a3f830c28e2f2ff",
+    "url": "/static/media/Robotic4.e384794d.jpg"
+  },
+  {
+    "revision": "acbd58b354fe40a9b75926dd71006c11",
+    "url": "/static/media/Shear.acbd58b3.png"
+  },
+  {
+    "revision": "fa145c7ce77a21c7adeeec552b8d147f",
+    "url": "/static/media/TIG1.fa145c7c.jpg"
+  },
+  {
+    "revision": "589b173c4783969384be815933c3c4bc",
+    "url": "/static/media/TIG2.589b173c.jpg"
+  },
+  {
+    "revision": "0e25a44b4119e855420907b5ca80d7eb",
+    "url": "/static/media/TIG3.0e25a44b.jpg"
+  },
+  {
+    "revision": "c0afadab76d6d3826cd5113d3102df35",
+    "url": "/static/media/apex.c0afadab.png"
+  },
+  {
+    "revision": "15234f22facdd96df616755325d38bdb",
+    "url": "/static/media/autocad-design.15234f22.jpg"
+  },
+  {
+    "revision": "d2fa87fdd818601b45f6c32287c0766f",
+    "url": "/static/media/fab-bg.d2fa87fd.jpg"
+  },
+  {
+    "revision": "d5a0a4782d432c5c0462200929264e6d",
+    "url": "/static/media/flex-drill.d5a0a478.png"
+  },
+  {
+    "revision": "a68fd982c6f12fc13b1f2edb07547403",
+    "url": "/static/media/flex.a68fd982.jpg"
+  },
+  {
+    "revision": "83902923ef85163765eff51d2997a1bd",
+    "url": "/static/media/fusion-design.83902923.jpg"
+  },
+  {
+    "revision": "e713e03e1ae457137869791741cb23f0",
+    "url": "/static/media/laser-bed-inch-small.e713e03e.jpg"
+  },
+  {
+    "revision": "38e1bab235a45509d5ccf75c145f8a59",
+    "url": "/static/media/laser-img.38e1bab2.png"
+  },
+  {
+    "revision": "a22a08ed94e90e9598fa04e56ac287fb",
+    "url": "/static/media/laser.a22a08ed.jpg"
+  },
+  {
+    "revision": "06294dcfa6945b53fc4d4d5c1abed0fb",
+    "url": "/static/media/laser1.06294dcf.jpg"
+  },
+  {
+    "revision": "761999c34098a5c4d8341adeeba61798",
+    "url": "/static/media/laser2.761999c3.jpg"
+  },
+  {
+    "revision": "5b14b8a591d6a29fd599e6c18bbd81f9",
+    "url": "/static/media/laser3.5b14b8a5.jpg"
+  },
+  {
+    "revision": "9666936c7a303db2644b2aaa1a5d34a0",
+    "url": "/static/media/laser4.9666936c.jpg"
+  },
+  {
+    "revision": "d13b0a27710881f9b331472953afbe99",
+    "url": "/static/media/laser5.d13b0a27.jpg"
+  },
+  {
+    "revision": "9ff9fed8578b524a202d1c96f9c75393",
+    "url": "/static/media/laser6.9ff9fed8.jpg"
+  },
+  {
+    "revision": "95402dbaf2fb596aaf5d18598b0aed28",
+    "url": "/static/media/laser7.95402dba.jpg"
+  },
+  {
+    "revision": "4130653d0071364c65823e10c20271e6",
+    "url": "/static/media/laser8.4130653d.jpg"
+  },
+  {
+    "revision": "ef5db73ce29c514bdc1cdfa426538512",
+    "url": "/static/media/press-brake.ef5db73c.jpg"
+  },
+  {
+    "revision": "d80cce7027e40751ca860fa0fb68ede0",
+    "url": "/static/media/press-design.d80cce70.png"
+  },
+  {
+    "revision": "b334d6d7aecbc3350a69519fda6d0670",
+    "url": "/static/media/press1.b334d6d7.jpg"
+  },
+  {
+    "revision": "3963e54ec7d835dfce420aa462166899",
+    "url": "/static/media/press2.3963e54e.jpg"
+  },
+  {
+    "revision": "2700cd70b2d1c7055716430f6347285d",
+    "url": "/static/media/press3.2700cd70.jpg"
+  },
+  {
+    "revision": "108db46f91e641710a66149222d006d3",
+    "url": "/static/media/press4.108db46f.jpg"
+  },
+  {
+    "revision": "a4cd42766d31fc1a6871a7d2f050f0be",
+    "url": "/static/media/press5.a4cd4276.jpg"
+  },
+  {
+    "revision": "674606f6c9b3e745afeac36da5a6c64c",
+    "url": "/static/media/press6.674606f6.jpg"
+  },
+  {
+    "revision": "66175334aca0c6df771e4a8316a4ddb1",
+    "url": "/static/media/press7.66175334.jpg"
+  },
+  {
+    "revision": "9dbfdc1cfdf0e1dae5ff45f9a3c9face",
+    "url": "/static/media/press8.9dbfdc1c.jpg"
+  },
+  {
+    "revision": "8c49254584c421203dd391310de5c5b4",
+    "url": "/static/media/robotic-welder.8c492545.png"
+  },
+  {
+    "revision": "85a1d1a507925209d3265c382e4196ef",
+    "url": "/static/media/roller.85a1d1a5.png"
+  },
+  {
+    "revision": "c7712bc400c4ea41da923451768bfea7",
+    "url": "/static/media/saw.c7712bc4.png"
+  },
+  {
+    "revision": "b7ad25c3b10eae70489d447f47b2ca0a",
+    "url": "/static/media/solidWorks-design.b7ad25c3.jpg"
+  },
+  {
+    "revision": "ad5c206894155c72182b72c59ad1477d",
+    "url": "/static/media/welder.ad5c2068.jpg"
+  }
+]);
